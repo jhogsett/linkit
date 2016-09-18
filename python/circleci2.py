54f669096b1cbf25c26b1011058bdc09b89cd1bf
Jerry Hogsett <jhogsett@gmail.com>
2016-09-18 19:43:18 +0000

    add more states; created another version reporting more info

--- python/circleci2.py
+++ python/circleci2.py
@@ -5,7 +5,7 @@ import json
 import sys
 
 token = sys.argv[1]
-url = 'https://circleci.com/api/v1.1/recent-builds?circle-token=' + token + '&limit=72&offset=0'
+url = 'https://circleci.com/api/v1.1/recent-builds?circle-token=' + token + '&limit=18&offset=0'
 
 s = None
 def setup(): 
@@ -24,25 +24,40 @@ def loop():
   switcher = {
     "failed": "red\0",
     "success": "green\0",
+    "finished": "green\0",
     "fixed": "blue\0",
     "no_tests": "white\0",              
     "retried": "orange\0", 
-    "timedout":"cyan\0",
+    "timedout":"red\0",
     "canceled":"yellow\0",
-    "not_run":"black\0",
+    "not_run":"white\0",
     "running":"pink\0",
     "queued":"magenta\0",
-    "scheduled":"magenta\0",
-    "not_running":"black\0",
+    "scheduled":"purple\0",
+    "not_running":"white\0",
     "infrastructure_fail":"cyan\0"             
   }
 
-  for x in range(0, 72):
-    st = j[71-x]['status'].encode('ascii', 'ignore')
-    color = switcher.get(st, "pink\0")
+  for x in range(0, 18):
+    lc = j[17-x]['lifecycle'].encode('ascii', 'ignore')
+    oc = j[17-x]['outcome'].encode('ascii', 'ignore')
+    st = j[17-x]['status'].encode('ascii', 'ignore')
+
+    color = switcher.get(lc, "black\0")
     s.write(color.encode()) 
     time.sleep(0.01)
 
+    color = switcher.get(oc, "black\0")
+    s.write(color.encode())
+    time.sleep(0.01)
+
+    color = switcher.get(st, "black\0")
+    s.write(color.encode())
+    time.sleep(0.01)
+
+    color = "black\0"
+    s.write(color.encode())
+    time.sleep(0.01)
   time.sleep(60)
 
 if __name__ == '__main__': 
