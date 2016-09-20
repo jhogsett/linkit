29f6700ea5137b1753bf9a822ce9c440bebeb8bf
Jerry Hogsett <jhogsett@gmail.com>
2016-09-20 07:45:23 +0000

    support for breathe effect

--- python/circleci4.py
+++ python/circleci4.py
@@ -23,12 +23,12 @@ switcher = {
   "timedout":"yellow\0blink",            
   "canceled":"pink",                   
   "not_run":"gray\0blink",             
-  "running":"ltblue\0blink",               
-  "queued":"magenta",                
+  "running":"ltblue\0breathe",               
+  "queued":"magenta\0breathe",                
   "scheduled":"purple",           
   "not_running":"white\0blink",          
   "infrastructure_fail":"white\0blink",
-  "none":"ltblue\0blink",                    
+  "missing":"ltblue\0blink",                    
   "spacer":"black"                       
 }   
 
@@ -37,51 +37,59 @@ def setup():
   s = serial.Serial("/dev/ttyS0", 57600) 
   s.write("erase\0".encode())
 
-def command(cmd_text):                     
-  color = switcher.get(cmd_text, "black")  
-  s.write((color + '\0').encode())         
-  time.sleep(0.01)  
-                                           
+inter_command_delay = 0.01
+
+def command(cmd_text):
+  s.write((cmd_text + '\0').encode())   
+  time.sleep(inter_command_delay)                       
+
+def color_command(color_cmd_text):                     
+  color_cmd = switcher.get(color_cmd_text, "black")  
+  command(color_cmd)
+                                          
 def spacer():                              
-  command("spacer") 
+  color_command("spacer") 
 
 def loop():
   r = requests.get(url)
   r = r.text.encode('utf-8')
   j = json.loads(r)
 
-#print r.status_code
-#print r.headers['content-type']
+  command("pause");
 
   for x in range(0, job_limit):
+
     lc = j[max_job - x]['lifecycle']
     oc = j[max_job - x]['outcome']
     st = j[max_job - x]['status']
 
     if lc is not None:
       lc = lc.encode('ascii', 'ignore')  
-      command(lc)
+      color_command(lc)
     else:
-      command("none")
+      color_command("missing")
 
     if oc is not None:                                  
       oc = oc.encode('ascii', 'ignore')                                                                                                                                                                    
-      command(oc)                            
+      color_command(oc)                            
     else:                                            
-      command("none") 
+      color_command("missing") 
       
     if st is not None:
       st = st.encode('ascii', 'ignore')                                                                                                                                                                    
-      command(st)
+      color_command(st)
     else:                                            
-      command("none") 
+      color_command("missing") 
     
     spacer()  
 
-  s.write("flush\0".encode())                                                                   
+  command("continue");
+  command("flush");
+
   time.sleep(request_frequency)
 
 if __name__ == '__main__': 
   setup() 
-  while True: 
+  while True:
     loop()
+ 
