97160eabfa655106054b0c74ca52c67ed750354a
Jerry Hogsett <jhogsett@gmail.com>
2016-11-17 02:51:45 +0000

    break up into an arbitrary number of even colors

--- python/demo4.py
+++ python/demo4.py
@@ -6,7 +6,6 @@ import random
 
 s = None
 
-num_leds = 64
 play_time = 5.0
 
 def flush_input():                        
@@ -18,14 +17,14 @@ def wait_for_ack():
   s.read(s.inWaiting())                   
 
 def command(cmd_text):
-  s.write((cmd_text + ':').encode())   
+  s.write((cmd_text + '|').encode())   
   wait_for_ack()
  
 def setup(): 
   global s 
-  s = serial.Serial("/dev/ttyS0", 115200) 
+  s = serial.Serial("/dev/ttyS0", 57600) 
   flush_input()
-  command("continue:pause:64:window:erase")
+  command("continue|pause|64|window|erase")
 
 size_range_min = 1
 size_range_max = 8
@@ -35,18 +34,17 @@ colors = [ "red", "orange", "yellow", "ltgreen", "green", "seafoam", "cyan", "lt
 
 def loop():
   osize = random.randrange(size_range_min, size_range_max) + 1
-  times = int(num_leds / osize)
-  times = random.randrange(1, times)
-  for i in range(0, times):
-    color = colors[random.randrange(0, num_colors)]                   
-    command(str(osize) + "," + str(osize * (times - i))  + ":pshifto")          
-    cmd = ""                                                          
-    cmd = cmd + str(osize * (times - i)) + ":window:"                              
-    cmd = cmd + color + ":effectr:" + str(osize - 1) + ":repeat:black"
-    command(cmd)               
-  command("64:window:continue")                                       
-  time.sleep(play_time)                                               
-  command("pause")  
+  for i in xrange(0, 63, osize):
+    color = colors[random.randrange(0, num_colors)]
+    command(str(osize) + "," + str((63-i) + 1) + "|pshifto")
+    cmd = ""
+    if(i < 63):
+      cmd = cmd + str(63-i) + "|window|"
+    cmd = cmd + color + "|effectr|" + str(osize - 1) + "|repeat|black"    
+    command(cmd)
+  command("64|window|continue")
+  time.sleep(play_time)
+  command("pause")
 
 if __name__ == '__main__': 
   setup() 
