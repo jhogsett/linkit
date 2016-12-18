fe1ff23402eab86f5b36153f4893d16a7ee57085
Jerry Hogsett <jerry@usertesting.com>
2016-12-17 17:45:07 -0800

    nailed a perfect equal power crossfade

---
+++ Arduino/libraries/ColorMath/test.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/ruby
+
+print "\x1b[2J\x1b[1;1H"
+
+@times = 20
+@crossfade = []
+@c1_last = 0
+@c1 = 10
+@c2 = 90
+
+def generate_equal_power_cross_fade(times)
+  (0..times).each do |i|
+    position = i / (times * 1.0)
+    @crossfade[i] = (0.5 - 0.5 * Math.cos( Math::PI * position)) ** 0.5
+  end
+
+  (0..times).each do |i|
+    puts "-" * (50 * @crossfade[i]) + "+" * (50 * @crossfade[@times - i])
+  end
+end
+
+# reconstruct previous c1 by undoing math
+def do_one_step_3(step)
+  if step > 1
+    prev_step = step - 1
+    prevc2 = @c2 * @crossfade[prev_step]
+    restored_c1 = (@c1 - prevc2) / @crossfade[@times - prev_step]
+    @c1 = restored_c1
+  end
+
+  newc1 = @c1 * @crossfade[@times - step]
+  newc2 = @c2 * @crossfade[step]
+  @c1 = newc1 + newc2
+
+  puts "-" * newc1 + "+" * newc2
+  #puts "*" * @c1
+end
+
+
+
+def doit
+  (0..@times).each do |i|
+    do_one_step_3(i)
+  end
+end
+
+generate_equal_power_cross_fade(@times)
+doit
+
