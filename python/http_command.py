424549480a583bab5a4558e280e7d6600ee81454
Jerry Hogsett <jhogsett@gmail.com>
2016-12-27 04:21:21 +0000

    add support for serving css

--- python/http_command.py
+++ python/http_command.py
@@ -9,12 +9,17 @@ import sys
 from subprocess import call
 import os
 from socket import error as socket_error
+import os.path
 
-global webpage
+global webpage, base_path, last_run
+
+last_run = ''
+
+base_path = os.getcwd() + '/'
 if len(sys.argv) > 1:
   webpage = sys.argv[1]
 else:
-  webpage = os.getcwd() + '/http_command.html'
+  webpage = base_path + 'http_command.html'
 
 global s
 s = None                            
@@ -29,6 +34,13 @@ def command(cmd_text):
   wait_for_ack()   
 
 class Handler(BaseHTTPRequestHandler):
+
+  def serve_page(self, page):
+    f = open(page, 'r')                       
+    self.wfile.write(f.read())                   
+    f.close  
+    self.wfile.close() 
+
   def do_GET(self):
     req = urlparse.urlparse(self.path)
 
@@ -38,25 +50,41 @@ class Handler(BaseHTTPRequestHandler):
       self.end_headers()
       args = urlparse.parse_qs(req.query)            
 
-#      print args
-
       if 'cmd' in args:
         command("::pause")
         for cmd in args['cmd']:
           command(cmd)           
         command("flush:continue")
 
+      if 'run' in args:
+        if last_run != '':
+          print 'killing: ' + last_run
+          call('killall ' + last_run, shell=True)
+        last_run = args['run'][0] 
+        run = base_path + last_run
+        print 'running: ' + run
+        call(run, shell=True)
+
       if 'sys' in args:
         for sys in args['sys']:
-          print sys
+          print 'shell command: ' + sys
           call(sys, shell=True)
 
-      f = open(webpage, 'r')                    
-      self.wfile.write(f.read())  
-      f.close                     
-        
-      self.wfile.close()
+      self.serve_page(webpage)
 
+#      f = open(webpage, 'r')                    
+#      self.wfile.write(f.read())  
+#      f.close                     
+#      self.wfile.close()
+
+    elif os.path.isfile(base_path + req.path):
+      self.send_response(200)              
+      self.send_header("Content-type", "text/css")
+      self.end_headers()                           
+
+      page = base_path + req.path 
+      print 'serving page: ' + page
+      self.serve_page(page)
     else:
       self.send_response(404)
       self.send_header("Content-type", "text/html")               
