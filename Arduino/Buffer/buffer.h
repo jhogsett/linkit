5273b0a8051051764432ab14d98e2649f9ee9d81
Jerry Hogsett <jerry@usertesting.com>
2017-07-09 17:41:48 -0700

    some code clean up around rotation

---
+++ Arduino/Buffer/buffer.h
@@ -0,0 +1,375 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+#include <PololuLedStrip.h>
+#include <render.h>
+#include <zones.h>
+
+#define LEAVE_EFFECT -1
+#define NO_EFFECT 0
+
+#define OVERRIDE_OFF -1
+
+class Buffer
+{
+  public:
+  void begin(PololuLedStripBase **ledStrips, byte default_brightness, byte safety_led_count, byte visible_led_count, Render *renderer, rgb_color **buffers, rgb_color *render, byte **effects_buffers, Zones * zones);
+
+  void display_buffer(rgb_color * pbuffer);
+  void render_display();
+  void erase(bool display);
+  void push_color(rgb_color color, byte times, bool display, byte effect, byte max, byte start);
+  void push_rgb_color(byte red, byte green, byte blue);
+  void push_hsl_color(int hue, int sat, int lit);
+  void push_carry_color();
+  void shift(byte count, byte maxx, bool fast_render);
+  void finalize_shift(byte count, byte max);
+  // void set_color(byte pos, rgb_color color, bool display, byte effect);
+  void fade(float rate);
+  void fade_fast();
+  void cross_fade(byte step);
+  byte get_window();
+  void set_display(byte display);
+  void set_buffer(byte buffer);
+  rgb_color * get_buffer();
+  byte * get_effects_buffer();
+  byte get_current_display();
+  rgb_color * get_render_buffer();
+  void set_offset_override(byte offset);
+  void set_window_override(byte window);
+  byte get_offset();
+  byte get_width();
+  void set_zone(byte zone);
+  void reset();
+  void set_reverse(bool reverse);
+  bool get_reverse();
+  void rotate();
+  byte get_display();
+  byte get_zones();
+  void set_black_level(rgb_color black_level);
+  void reset_black_level();
+  rgb_color black;
+  int get_default_brightness();
+
+  // todo: is there an alternative to storing all these pointers?
+  private:
+  PololuLedStripBase **ledStrips;
+  byte current_display;
+  rgb_color **buffers;
+  static rgb_color *render;
+  byte **effects_buffers;
+  float default_brightness_scale;
+  Render *renderer;
+  byte safety_led_count;
+  byte visible_led_count;
+  rgb_color carry_color;
+  byte carry_effect;
+
+  int window_override = OVERRIDE_OFF;
+  int offset_override = OVERRIDE_OFF;
+
+  Zones *zones;
+  byte current_zone = 0;
+  bool reverse = false;
+
+  void shift_buffer(rgb_color * buffer, byte * effects, byte max, byte start, bool reverse);
+};
+
+rgb_color *Buffer::render;
+
+void Buffer::begin(PololuLedStripBase **ledStrips, byte default_brightness, byte safety_led_count, byte visible_led_count, Render *renderer, rgb_color **buffers, rgb_color *render, byte **effects_buffers, Zones * zones){
+  this->black = BLACK;
+  this->ledStrips = ledStrips;
+  this->current_display = 0;
+  this->buffers = buffers;
+  this->render = render;
+  this->default_brightness_scale = default_brightness / 100.0;
+  this->effects_buffers = effects_buffers;
+  this->renderer = renderer;
+  this->safety_led_count = safety_led_count;
+  this->visible_led_count = visible_led_count;
+  this->window_override = OVERRIDE_OFF;
+  this->offset_override = OVERRIDE_OFF;
+  this->zones = zones;
+  this->reverse = false;
+  this->carry_color = black;
+  this->carry_effect = NO_EFFECT;
+}
+
+byte Buffer::get_display(){
+  return this->current_display;
+}
+
+void Buffer::reset(){
+  //this->reset_black_level();
+  this->set_zone(0);
+  this->set_display(0);
+  this->set_reverse(false);
+}
+
+// always write from the render buffer to a pin,
+// the render buffer having been rendered from a specific display buffer already
+void Buffer::display_buffer(rgb_color * pbuffer){
+  ledStrips[current_display]->write(pbuffer, visible_led_count);
+}
+
+// todo might want choice of slow or fast render
+void Buffer::render_display(){
+  renderer->render_buffer(render, this->buffers[current_display], visible_led_count, this->effects_buffers[current_display]);
+  display_buffer(render);
+}
+
+void Buffer::erase(bool display = false)
+{
+  byte offset = get_offset();
+  byte window = get_window();
+  rgb_color * buf = buffers[current_display];
+  byte * effects = effects_buffers[current_display];
+  for(byte i = offset; i < window; i++){
+    buf[i] = black;
+    effects[i] = NO_EFFECT;
+  }
+
+  if(display){
+    render_display();
+  }
+}
+
+void Buffer::cross_fade(byte step){
+  byte offset = get_offset();
+  byte window = get_window();
+  rgb_color *buffer = buffers[current_display];
+  byte* effects = effects_buffers[current_display];
+
+  for(byte i = offset; i < window; i++){
+    rgb_color *pb = buffer + i;
+    rgb_color *pr = render + i;
+    rgb_color rendered_color = renderer->render(pb, effects[i]);
+    *pr = ColorMath::crossfade_colors(step, *pr, rendered_color);
+   }
+}
+
+void Buffer::rotate(){
+  rgb_color * buffer = buffers[current_display];
+  byte * effects = effects_buffers[current_display];
+  byte max = get_window();
+  byte start = get_offset();
+
+  shift_buffer(buffer, effects, max, start, this->reverse);
+
+  if(reverse){
+    buffer[max - 1] = this->carry_color;
+    effects[max - 1] = this->carry_effect;
+  } else {
+    buffer[start] = this->carry_color;
+    effects[start] = this->carry_effect;
+  }
+}
+
+void Buffer::shift_buffer(rgb_color * buffer, byte * effects, byte max, byte start, bool reverse = false){
+  if(reverse){
+    this->carry_color = buffer[start];
+    this->carry_effect = effects[start];
+
+    for(byte i = start; i < (max - 1); i++){
+      buffer[i] = buffer[i+1];
+      effects[i] = effects[i+1];
+    }
+  } else {
+    this->carry_color = buffer[max - 1];
+    this->carry_effect = effects[max - 1];
+
+    for(byte i = max - 1; i >= (start + 1); i--){
+      buffer[i] = buffer[i-1];
+      effects[i] = effects[i-1];
+    }
+  }
+}
+
+// todo: is the ability to set the effect here ever used?
+// yes with pushing carry color+effect
+void Buffer::push_color(rgb_color color, byte times = 1, bool display = false, byte effect = NO_EFFECT, byte max = 0, byte start = 0)
+{
+  rgb_color * buffer = buffers[current_display];
+  byte * effects = effects_buffers[current_display];
+
+  max = (max == 0) ? get_window() : max;
+  start = (start == 0) ? get_offset() : start;
+
+  times = max(1, times);
+
+  for(byte i = 0; i < times; i++){
+    shift_buffer(buffer, effects, max, start, this->reverse);
+
+    if(this->reverse){
+      buffer[max-1] = ColorMath ::correct_color(color);
+      effects[max-1] = effect;
+    } else {
+      buffer[start] = ColorMath::correct_color(color);
+      effects[start] = effect;
+    }
+
+    if(display){
+      render_display();
+    }
+  }
+}
+
+void Buffer::push_rgb_color(byte red, byte green, byte blue){
+  rgb_color color = (rgb_color){red, green, blue};
+  color = ColorMath::unscale_color(color, default_brightness_scale);
+  push_color(color);
+}
+
+void Buffer::push_hsl_color(int hue, int sat, int lit){
+  rgb_color color = ColorMath::hsl_to_rgb(hue, sat, lit);
+  push_rgb_color(color.red, color.green, color.blue);
+}
+
+void Buffer::push_carry_color(){
+  push_color(carry_color, 1, false, carry_effect);
+}
+
+// todo: is this used?
+//void Buffer::set_color(byte pos, rgb_color color, bool display = false, byte effect = NO_EFFECT)
+//{
+//  rgb_color * buffer = buffers[current_display];
+//  byte * effects = effects_buffers[current_display];
+//
+//  buffer[pos] = ColorMath::correct_color(color);
+//
+//  if(effect != LEAVE_EFFECT){
+//    effects[pos] = effect;
+//  }
+//
+//  if(display){
+//    render_display();
+//  }
+//}
+
+void Buffer::set_window_override(byte window){
+  if(window < 0){
+    this->window_override = OVERRIDE_OFF;
+  } else {
+    this->window_override = max(1, min(visible_led_count + 1, window));
+  }
+}
+
+byte Buffer::get_window(){
+  if(this->window_override != OVERRIDE_OFF){
+    return this->window_override;
+  } else {
+    return this->zones->zone_window(this->current_zone);
+  }
+}
+
+void Buffer::set_offset_override(byte offset){
+  if(offset < 0){
+    this->offset_override = OVERRIDE_OFF;
+  } else {
+    this->offset_override = max(0, min(visible_led_count, offset));
+  }
+}
+
+byte Buffer::get_offset(){
+  if(this->offset_override != OVERRIDE_OFF){
+    return this->offset_override;
+  } else {
+    return this->zones->zone_offset(this->current_zone);
+  }
+}
+
+byte Buffer::get_width(){
+  return this->get_window() - this->get_offset();
+}
+
+void Buffer::set_zone(byte zone){
+  this->current_zone = max(0, min(this->get_zones() - 1, zone));
+  this->window_override = OVERRIDE_OFF;
+  this->offset_override = OVERRIDE_OFF;
+}
+
+void Buffer::set_display(byte display){
+  this->current_display = display;
+}
+
+byte Buffer::get_current_display(){
+  return this->current_display;
+}
+
+rgb_color * Buffer::get_buffer(){
+  return this->buffers[current_display];
+}
+
+byte * Buffer::get_effects_buffer(){
+  return this->effects_buffers[current_display];
+}
+
+rgb_color * Buffer::get_render_buffer(){
+  return this->render;
+}
+
+void Buffer::set_reverse(bool reverse = true){
+  this->reverse = reverse;
+}
+
+bool Buffer::get_reverse(){
+  return this->reverse;
+}
+
+byte Buffer::get_zones(){
+  return this->zones->get_num_zones();
+}
+
+void Buffer::set_black_level(rgb_color black_level){
+  this->black = black_level;
+}
+
+void Buffer::reset_black_level(){
+  this->black = BLACK;
+}
+
+int Buffer::get_default_brightness(){
+  return (int)(this->default_brightness_scale * 100.0);
+}
+
+// to do: support either orientation
+// to do: restrict to current zone
+// animate by shifting frame (rendering frame
+// buffer in different render buffer positions)
+void Buffer::shift(byte count, byte maxx, bool fast_render = true){
+  rgb_color * buffer = buffers[current_display];
+  byte * effects = effects_buffers[current_display];
+
+  // to do: restrict to visible led count?
+  maxx = min(maxx, safety_led_count);
+
+  // this is used repeatedly during a shift animation to render the
+
+  // to do: start off offset
+  // to do: would it be faster to set each individual byte?
+  for(byte i = 0; i < count; i++){
+    this->render[i] = black;
+  }
+
+  // to do: add offset
+  for(byte i = count; i < maxx; i++){
+    if(fast_render)
+      this->render[i] = renderer->fast_render(buffer[i - count], effects[i - count]);
+    else
+      this->render[i] = renderer->render(&buffer[i - count], effects[i - count]);
+  }
+
+  display_buffer(this->render);
+}
+
+void Buffer::finalize_shift(byte count, byte max){
+
+  // to do: restrict to current zone
+//  for(byte i = 0; i < count; i++){
+//    push_color(black, 1, false, NO_EFFECT, max);
+//  }
+  push_color(black, count, false, NO_EFFECT, max);
+}
+
+#endif
---
+++ Arduino/CommandProcessor/command_processor.h
@@ -0,0 +1,212 @@
+#ifndef COMMAND_PROCESSOR_H
+#define COMMAND_PROCESSOR_H
+
+#define DELIMITER_CHAR ':'
+#define DELIMITER_STR ":"
+#define MAX_STRING_LENGTH 60
+#define NUM_SUB_ARGS 3
+#define CMD_NULL -1
+#define CMD_NONE 0
+#define CMD_FIRST 1
+
+#define SUB_ARGS_FIX
+
+class CommandProcessor
+{
+  public:
+
+  char str[MAX_STRING_LENGTH];
+
+  int sub_args[NUM_SUB_ARGS];
+  int accumulator = 0;
+
+  const char * const *commands;
+
+  byte num_commands;
+  HardwareSerial *serial;
+
+  void begin(HardwareSerial *serial, const char* const *commands, byte num_commands);
+
+  bool input_available();
+  bool received_command();
+  void acknowledge_command(bool force);
+  void send_ack();
+  int lookup_command(char * str);
+  int get_command();
+  void save_args(char * str);
+  void reset_args();
+  int process_command(char * str);
+  char * begin_get_commands(char *buffer, char **saveptr);
+  char * get_next_command(char **saveptr);
+  char * get_input_buffer();
+  void get_sub_args(char * str);
+  char * borrow_char_buffer();
+  void send_int(int value);
+  void send_ints(int value);
+  void send_str(char * value);
+
+  private:
+
+  bool str_equal_P(char *str1, const char *str2);
+  bool is_command_P(char *str, const char *command);
+  void save_accumulator();
+};
+
+void CommandProcessor::begin(HardwareSerial *serial, const char* const *commands, byte num_commands){
+  this->serial = serial;
+  this->commands = commands;
+  this->num_commands = num_commands;
+  this->serial = serial;
+
+  // force a command acknowledgement to wake up any script that may be halted
+  // waiting for a character to be sent due to a new Arduino sketch being uploaded
+  acknowledge_command(true);
+}
+
+bool CommandProcessor::input_available(){
+  return serial->available() > 0;
+}
+
+// read a single command into the in-memory buffer
+// this does not need to be re-entrant
+// the effective buffer size may be this buffer width + 64-char serial buffer input size
+bool CommandProcessor::received_command(){
+  if(input_available()){
+    byte c = serial->readBytesUntil(DELIMITER_CHAR, str, MAX_STRING_LENGTH-1);
+    this->str[c] = 0;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// only acknowledge when there are no more commands available
+// so that multiple additional commands if any are processed first
+void CommandProcessor::acknowledge_command(bool force = false){
+  if(!input_available() || force)
+    send_ack();
+}
+
+void CommandProcessor::send_ack(){
+  serial->write("k");
+}
+
+void CommandProcessor::send_int(int value){
+  serial->print(value);
+}
+
+#define INT_DELIMITER_CHAR ','
+
+void CommandProcessor::send_ints(int value){
+  serial->print(value);
+  serial->print(INT_DELIMITER_CHAR);
+}
+
+void CommandProcessor::send_str(char * value){
+  serial->print(value);
+}
+
+void CommandProcessor::save_args(char * args = NULL){
+  get_sub_args(args);
+}
+
+// str2 is a pointer to a string in PROGMEM
+bool CommandProcessor::str_equal_P(char *str1, const char *str2){
+  for(byte i = 0; i < MAX_STRING_LENGTH; i++){
+    char c1 = *(str1 + i);
+    char c2 = (char)pgm_read_byte(str2 + i);
+    if(c1 == 0 && c2 == 0) return true;
+    if(c2 != c1) return false;
+  }
+  return false;
+}
+
+bool CommandProcessor::is_command_P(char *str, const char *command){
+  return str_equal_P(str, command);
+}
+
+void CommandProcessor::get_sub_args(char * args = NULL){
+  save_accumulator();
+
+  if(args == NULL){
+    args = this->str;
+  }
+
+  char *saveptr;
+  char *token = strtok_r(args, ",", &saveptr);
+  sub_args[0] = atoi(token);
+  token = strtok_r(NULL, ",", &saveptr);
+  sub_args[1] = atoi(token);
+  token = strtok_r(NULL, ",", &saveptr);
+  sub_args[2] = atoi(token);
+}
+
+void CommandProcessor::reset_args(){
+  save_accumulator();
+
+  sub_args[0] = 0;
+  sub_args[1] = 0;
+  sub_args[2] = 0;
+}
+
+void CommandProcessor::save_accumulator(){
+  accumulator = sub_args[0];
+}
+
+int CommandProcessor::lookup_command(char * str){
+  if(str == NULL){
+    return CMD_NULL;
+  }
+
+  for(byte i = 0; i < num_commands; i++){
+    if(is_command_P(str, (char*)pgm_read_word(&(commands[i])))){
+      return CMD_FIRST + i;
+    }
+  }
+  return CMD_NONE;
+}
+
+// look up a command copied to the input buffer
+int CommandProcessor::get_command(){
+  return lookup_command(this->str);
+}
+
+// process a command from a string or macro
+// this ensures arguments are parsed and saved
+int CommandProcessor::process_command(char * str){
+  int command = lookup_command(str);
+
+  // if CMD_NONE is returned, the command wasn't found; parse the string for arguments
+  // the default save_args() should not be called by; the calling code in this case
+  if(command == CMD_NONE){
+    save_args(str);
+  }
+
+  return command;
+}
+
+char * CommandProcessor::begin_get_commands(char * buffer, char **saveptr){
+  return strtok_r(buffer, DELIMITER_STR, saveptr);
+}
+
+char * CommandProcessor::get_next_command(char **saveptr){
+  return strtok_r(NULL, DELIMITER_STR, saveptr);
+}
+
+// read the contents serial buffer into the char buffer
+// this is used for setting macros that come in over serial
+char * CommandProcessor::get_input_buffer(){
+  if(input_available()){
+    int c = serial->readBytesUntil('\0', this->str, MAX_STRING_LENGTH-1);
+    this->str[c] = 0;
+    return this->str;
+  } else {
+    return NULL;
+  }
+}
+
+char * CommandProcessor::borrow_char_buffer(){
+  return this->str;
+}
+
+#endif
