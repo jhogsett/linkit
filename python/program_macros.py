0efa2166bde4c84b54cc971f48a3f73c7345b33f
Jerry Hogsett <jhogsett@gmail.com>
2017-08-21 04:39:30 +0000

    added hue sequence for annunciator

--- python/program_macros.py
+++ python/program_macros.py
@@ -108,28 +108,34 @@ def monument_macros():
 def annunciator_macros():
   print tc.cyan("annunciator macros:\n")
 
-  # main start
-  # pause effects, set up sequencers and fade, set up display macro schedules, set shuffle schedule
-  set_macro(10, "1:pau:16:run:21:run:31:run", 0)
+  # m
+  set_macro(10, "-1:sch:22:run", 0)
+  set_macro(11, "-1:sch:23:run", 0)
 
   # sequence the next window for each display
-  set_macro(11, "0:snw:pos:0:pal:sfd:flo:rst:flu", 0)
-  set_macro(12, "1:snw:pos:1:pal:sfd:flo:rst", 0)
-  set_macro(13, "2:snw:pos:2:pal:sfd:flo:rst", 0)
+  set_macro(13, "0:snw:pos:0:pal:sfd:flo:rst:flu", 0)
+  set_macro(14, "1:snw:pos:1:pal:sfd:flo:rst", 0)
+  set_macro(15, "2:snw:pos:2:pal:sfd:flo:rst", 0)
 
   # set up each sequencer
-  set_macro(14, "0,13,0:ssc:1,25,12:ssc", 0)
-  set_macro(15, "2,37,24:ssc", 0)
+  set_macro(16, "0,13,0:ssc:1,25,12:ssc", 0)
+  set_macro(17, "2,37,24:ssc", 0)
 
   # set up all sequences and a quick fade rate
-  set_macro(16, "14:run:15:run:3,8000:cfg", 0)
+  set_macro(18, "16:run:17:run:3,8000:cfg", 0)
 
   # set up each each display macro schedule
-  set_macro(17, "145,11:sch:150,12:sch", 0)
-  set_macro(18, "155,13:sch", 0)
+  set_macro(19, "145,13:sch:150,14:sch", 0)
+  set_macro(20, "155,15:sch", 0)
 
   # set up all display macro schedules
-  set_macro(21, "17:run:18:run", 0)
+  set_macro(21, "19:run:20:run", 0)
+
+  # start up random color change power swingers
+  set_macro(22, "1:pau:18:run:21:run:31:run", 0)
+
+  # start up hue sequence swingers
+  set_macro(23, "1:pau:18:run:21:run:32:run", 0)
 
   # set up shuffler
   set_macro(30, "shf", 1)
@@ -137,12 +143,9 @@ def annunciator_macros():
   # set shuffler schedule
   set_macro(31, "13000,30:sch", 0)
 
-
-
-
-
-
-
+  # set up sequencing hues
+  set_macro(32, "3,360:seq:500,33:sch", 0)
+  set_macro(33, "3,0,1:seq:sto:120:rcl:csh", 0)
 
 
 
