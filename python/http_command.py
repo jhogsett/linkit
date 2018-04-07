1fa11d6dd3eceaf2b6a4eb45af210ac4c8ee3e6e
Jerry Hogsett <jhogsett@gmail.com>
2018-04-07 15:32:13 +0000

    refactor into smaller methods

--- python/http_command.py
+++ python/http_command.py
@@ -16,37 +16,48 @@ import led_command as lc
 import argparse
 import app_ui as ui
 
-global httpd, webpage, base_path, last_run, host_name, host_ip
+global httpd, webpage, base_path, last_run, host_name, host_ip, app_description, verbose_mode, debug_mode, num_leds, macro_count, programs, macro_run_number, retry_wait
 
 last_run = ''
 last_run_full = ''
 host_name = socket.getfqdn(socket.gethostname())
 host_ip = socket.gethostbyname(socket.gethostname())
-
-if len(sys.argv) > 2:
-  base_path = sys.argv[2] + '/'
-else:
-  base_path = os.getcwd() + '/'
-
-if len(sys.argv) > 1:
-  webpage = sys.argv[1]
-else:
-  webpage = base_path + 'http_command.html'
-
-global app_description, verbose_mode, debug_mode, num_leds, macro_count, programs, macro_run_number
 app_description = None
 verbose_mode = None
 debug_mode = None
 num_leds = None
+retry_wait = None
+
+#if len(sys.argv) > 2:
+#  base_path = sys.argv[2] + '/'
+#else:
+#  base_path = os.getcwd() + '/'
+
+#if len(sys.argv) > 1:
+#  webpage = sys.argv[1]
+#else:
+#  webpage = base_path + 'http_command.html'
 
 def get_options():
-  global verbose_mode, debug_mode
+  global verbose_mode, debug_mode, retry_wait, base_path, webpage
   parser = argparse.ArgumentParser(description=app_description)
+  parser.add_argument("page", metavar="P", nargs="?", help="path to web page")
+  parser.add_argument("path", metavar="R", nargs="?", help="root path for files")
+  parser.add_argument("-r", "--retry", type=int, dest="retry", default=10, help='retry wait (secs) (10)')
   parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="display verbose info (False)")
   parser.add_argument("-d", "--debug", dest="debug", action="store_true", help="display verbose info (False)")
   args = parser.parse_args()
+  retry_wait = args.retry
   verbose_mode = args.verbose
   debug_mode = args.debug
+
+  base_path = args.path
+  if base_path == None:
+    base_path = os.getcwd() + '/'
+
+  webpage = args.page
+  if webpage == None:
+    webpage = base_path + 'http_command.html'
 
 def validate_options():
     errors = False
@@ -66,14 +77,15 @@ def initialize():
   lc.command("cnt")
 
 def introduction():
-    ui.app_description(app_description)
-
-    ui.report_verbose("verbose mode")
-    ui.report_verbose("debug_mode: " + str(debug_mode))
-    ui.report_verbose()
-
-    ui.report_info(ui.intro_entry("Number of LEDs", num_leds))
-    print 
+  ui.app_description(app_description)
+  ui.report_verbose("verbose mode")
+  ui.report_verbose("root path: " + base_path)
+  ui.report_verbose("web page: " + webpage)  
+  ui.report_verbose("retry wait: " + str(retry_wait) + "s")
+  ui.report_verbose("debug_mode: " + str(debug_mode))
+  ui.report_verbose()
+  ui.report_info(ui.intro_entry("Number of LEDs", num_leds))
+  print 
 
 class Handler(BaseHTTPRequestHandler):
   global last_run, last_run_full, host_name, host_ip, to_run
@@ -109,31 +121,45 @@ class Handler(BaseHTTPRequestHandler):
     print '#' * 80
     print 
 
+  def banner(self):
+    if last_run != '':
+      return """
+<div class="well well-sm clearfix">
+  <div class="col-xs-9"><i class="fa fa-circle-o-notch fa-spin fa-fw"></i> %(last_run)s</div>
+  <div class="col-xs-3"><a class="btn btn-link btn-xs pull-right" role="button" href="/command?run=stop"><span class="glyphicon glyphicon-remove-sign"></span></a></div>
+</div>
+      """ % globals()
+    return ''
+
+  def footer(self):
+    return """
+<div class="small text-center">
+  %(host_name)s %(host_ip)s
+</div>
+<div class="small text-center">
+  %(last_run_full)s
+</div>
+    """ % globals()
+
   def serve_page(self, page, headers={}):
     global last_run, last_run_full, host_name, host_ip
-
     filename, file_ext = os.path.splitext(page)
     filetime = datetime.datetime.fromtimestamp(os.path.getmtime(page))
     filetime_str = filetime.strftime("%a, %d %b %Y %H:%M:%S GMT")
-
     is_cached = False
     if headers != None:
       if 'If-Modified-Since' in headers:
         modified_since = headers['If-Modified-Since']
         if modified_since != None:
-
           print modified_since
           print filetime_str
-
           if modified_since.lower() == filetime_str.lower():
             is_cached = True
-
     if is_cached:
       self.send_response(304)
       self.send_header("Content-Length", "0")
       self.end_headers()
       return
-
     if file_ext == '.html':
       content_type = "text/html"                     
     elif file_ext == '.css':
@@ -146,44 +172,22 @@ class Handler(BaseHTTPRequestHandler):
       content_type = "image/png"
     else:
       content_type = "application/octet-stream"                     
-
     self.send_response(200)
     self.send_header("Content-type", content_type)
-
     # can't cache the html page until actions are backgrounded
     if content_type == "text/html":
       self.send_header("Cache-Control", "no-cache")
     else:
       self.send_header("Cache-Control", "private")
       self.send_header("Last-Modified", filetime_str)
-
     self.end_headers()  
-
-    if last_run != '':
-      banner = """
-<div class="well well-sm clearfix">
-  <div class="col-xs-9"><i class="fa fa-circle-o-notch fa-spin fa-fw"></i> %(last_run)s</div>
-  <div class="col-xs-3"><a class="btn btn-link btn-xs pull-right" role="button" href="/command?run=stop"><span class="glyphicon glyphicon-remove-sign"></span></a></div>
-</div>
-      """ % globals()
-    else:
-      banner = ''
-
-    footer = """
-<div class="small text-center">
-  %(host_name)s %(host_ip)s
-</div>
-<div class="small text-center">                                                                                                                                         
-  %(last_run_full)s                                                                                                                                             
-</div>                                                                                                                                                                  
-    """ % globals()
-
+    banner = self.banner()
+    footer = self.footer()
     f = open(page, 'r')                       
     if content_type == "text/html":
     	self.wfile.write(f.read().replace('<!-- banner -->', banner).replace('<!-- footer -->', footer))
     else:
         self.wfile.write(f.read())
-                   
     f.close  
     self.wfile.close() 
 
@@ -235,71 +239,60 @@ class Handler(BaseHTTPRequestHandler):
   def do_cast(self):
     pass
 
-  def do_GET(self):
-    req = urlparse.urlparse(self.path)
-    headers = {'If-Modified-Since': self.headers.getheader('If-Modified-Since')}
- 
-    if req.path == '/command':
-      args = urlparse.parse_qs(req.query)            
+  def get_headers(self):
+    return {'If-Modified-Since': self.headers.getheader('If-Modified-Since')}
 
-      if 'cmd' in args:
-        # if an app is running, stop it first
-        #self.stop_running_app()
-        #self.handle_commands(["3:pau"] + args['cmd'] + [":3:cnt"])
-        #self.restart_running_app()
-        self.do_cmd(args)
+  def do_webpage(self):
+    headers = self.get_headers()
+    # serve main page
+    # self.serve_page(webpage, headers)
+    # until command actions are handled asynchronously, need to
+    # serve the page each time non-cached
+    self.serve_page(webpage)
 
-      if 'color' in args:
-        # if an app is running, stop it first
-        #self.stop_running_app()
-        #self.handle_commands(["2:pau"] + args['color'] + [":1:cnt:flu"])
-        #self.restart_running_app()
-        self.do_color(args)
-
-      if 'macro' in args:
-        # if an app is running, stop it first
-        #self.stop_running_app()
-        #self.handle_commands(["1:pau:2:cnt"] + [str(args['macro'][0]) + ":run"])
-        #self.restart_running_app()
-        self.do_macro(args) 
-
-      if 'run' in args:
-        #self.kill_last_app() 
-        #self.run_app(args['run'][0])
-        self.do_run(args)
-
-      if 'runonce' in args:
-        #self.run_app(args['runonce'][0], False)
-        self.runonce(args)
-
-      if 'sys' in args:
-        #for sys in args['sys']:
-        #  self.log('shell command: ' + sys)
-        #  call(sys, shell=True)
-        do_sys(args)
-
-      if 'cast' in args:
-        do_cast(args)
-
-      # serve main page
-      # self.serve_page(webpage, headers)
-
-      # until command actions are handled asynchronously, need to
-      # serve the page each time non-cached
-      self.serve_page(webpage)
-
-    elif os.path.isfile(base_path + req.path):
-      page = base_path + req.path 
+  def do_file(self, url):
+    headers = self.get_headers()
+    if os.path.isfile(base_path + url.path):
+      page = base_path + url.path
       self.serve_page(page, headers)
-
+      return True
     else:
-      msg = "<html><body><h1>404 Not Found</h1></body></html>"
-      self.send_response(404)
-      self.send_header("Content-Type", "text/html")
-      self.send_header("Content-Length", len(msg))
-      self.end_headers()                             
-      self.wfile.write(msg)
-      self.wfile.close()                             
+      return False
+
+  def do_notfound(self):
+    msg = "<html><body><h1>404 Not Found</h1></body></html>"
+    self.send_response(404)
+    self.send_header("Content-Type", "text/html")
+    self.send_header("Content-Length", len(msg))
+    self.end_headers()
+    self.wfile.write(msg)
+    self.wfile.close()
+
+  def do_command(self, url):
+    args = urlparse.parse_qs(url.query)
+    if 'cmd' in args:
+      self.do_cmd(args)
+    if 'color' in args:
+      self.do_color(args)
+    if 'macro' in args:
+      self.do_macro(args)
+    if 'run' in args:
+      self.do_run(args)
+    if 'runonce' in args:
+      self.runonce(args)
+    if 'sys' in args:
+      self.do_sys(args)
+    if 'cast' in args:
+      self.do_cast(args)
+    self.do_webpage()
+
+  def do_GET(self):
+    url = urlparse.urlparse(self.path)
+    if url.path == '/command':
+      self.do_command(url)
+    else: 
+      if not self.do_file(url):
+        self.do_notfound()
 
 ############################################################################
 
@@ -310,7 +303,7 @@ def start_server():
       httpd = SocketServer.TCPServer(("", 8080), Handler)
     except socket_error,e:
       print "Error: " + str(e) + " - retrying"
-      time.sleep(5)
+      time.sleep(retry_wait)
       continue
     print "Listening..."
     break
