a6c1499269845cad552d1b2facf18fa2528ea75a
Jerry Hogsett <jhogsett@gmail.com>
2018-04-09 01:36:25 +0000

    add backgrounding of sent commands

--- python/udp_send.py
+++ python/udp_send.py
@@ -9,6 +9,7 @@ import time
 import fcntl
 import struct
 import app_ui as ui
+import threading
 
 app_description = "LED Multicast Sender v.0.0 10-1-2017"
 
@@ -80,48 +81,114 @@ ui.info_entry("sending keys", str(no_keys == False))
 ui.info_entry("message delay", str(msg_delay))
 print
 
+#def add_key(command):
+#    return server_name + "/" + str(time.time()) + ";" + command
+#
+#def send_message(message, times):
+#    if no_keys != True:
+#        message = add_key(message)
+#    for n in range(0, times):
+#        # Send data to the multicast group
+#        ui.report_verbose('sending "%s"' % message)
+#        sent = sock.sendto(message, multicast_group)
+#        if verbose_mode:
+#            while True:
+#                try:
+#                    data, server = sock.recvfrom(256)
+#                except socket.timeout:
+#                    break
+#                else:
+#                    ui.report_verbose_alt('received "%s" from %s' % (data, server))
+#        if n < (times - 1):
+#            time.sleep(msg_delay * (2 ** n))
+#if command != None:
+#    send_message(command, num_times)
+#    sock.close()
+#    exit(0);
+#
+#cmd = ""
+#last_cmd = cmd
+
+def cast_socket():
+  # Create the datagram socket
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+  # Set a timeout so the socket does not block indefinitely when trying
+  # to receive data.
+  sock.settimeout(timeout_in_seconds)
+
+  # Set the time-to-live for messages to 1 so they do not go past the
+  # local network segment.
+  ttl = struct.pack('b', 1)
+  sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+  return sock
+
 def add_key(command):
-    return server_name + "/" + str(time.time()) + ";" + command
+  return host_name + "/" + str(time.time()) + ";" + command
 
-def send_message(message, times):
-    if no_keys != True:
-        message = add_key(message)
-    for n in range(0, times):
-        # Send data to the multicast group
-        ui.report_verbose('sending "%s"' % message)
-        sent = sock.sendto(message, multicast_group)
-        if verbose_mode:
-            while True:
-                try:
-                    data, server = sock.recvfrom(256)
-                except socket.timeout:
-                    break
-                else:
-                    ui.report_verbose_alt('received "%s" from %s' % (data, server))
-        if n < (times - 1):
-            time.sleep(msg_delay * (2 ** n))
-if command != None:
-    send_message(command, num_times)
-    sock.close()
-    exit(0);
+def send_socket_message(sock, message, times):
+  if no_keys != True:
+    message = add_key(message)
+  for n in range(0, times):
+    # Send data to the multicast group
+    ui.report_verbose('sending "%s"' % message)
+    sent = sock.sendto(message, multicast_group)
+    if verbose_mode:
+      while True:
+        try:
+          data, server = sock.recvfrom(256)
+        #except KeyboardInterrupt:
+        #  break
+        except socket.timeout:
+          break
+        else:
+          ui.report_verbose('received "%s" from %s' % (data, server))
+    if n < (times - 1):
+      time.sleep(msg_delay * (2 ** n))
 
-cmd = ""
-last_cmd = cmd
+def send_message(message):
+  sock = cast_socket()
+  send_socket_message(sock, message, num_times)
+  sock.close()
+
+background_threads = []
+
+def handle_background_message(message):
+  send_message(message)
+  thread = threading.current_thread()
+  background_threads.remove(thread)
+  ui.report_verbose("terminating thread: " + str(thread))
+
+def send_background_message(message):
+  thread = threading.Thread(target=handle_background_message, args=(message,))
+  ui.report_verbose("new thread: " + str(thread))
+  background_threads.append(thread)
+  thread.start()
+
+def wait_for_active_threads():
+  if(len(background_threads) > 0):
+    ui.report_warn("waiting for active threads to terminate...")
+    for t in background_threads:
+      t.join()
 
 while True:
     try:
         cmd = raw_input(tc.yellow('command: '))
         if cmd == "":
-            send_message(last_cmd, num_times)
+            send_background_message(last_cmd)
+            #send_message(last_cmd, num_times)
         else:
-           send_message(cmd, num_times)
+           send_background_message(cmd)
+           #send_message(cmd, num_times)
            last_cmd = cmd
-
     except EOFError:
         sock.close()
         sys.exit("\nExiting...\n")
+        wait_for_active_threads()
     except KeyboardInterrupt:
         sock.close()
+        wait_for_active_threads()
         sys.exit("\nExiting...\n")
     except Exception:
         sock.close()
