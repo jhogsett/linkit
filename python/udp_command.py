ee2d5b252547c9b3002a5547ab17c022cd095329
Jerry Hogsett <jhogsett@gmail.com>
2018-04-15 20:25:38 +0000

    can't get ip address on start up

--- python/udp_command.py
+++ python/udp_command.py
@@ -89,12 +89,15 @@ def handle_command(cmd_text):
     return True
 
 def get_ip_address(ifname):
-  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  return socket.inet_ntoa(fcntl.ioctl(
-    s.fileno(),
-    0x8915,  # SIOCGIFADDR
-    struct.pack('256s', ifname[:15])
-  )[20:24])
+  try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+      s.fileno(),
+      0x8915,  # SIOCGIFADDR
+      struct.pack('256s', ifname[:15])
+    )[20:24])
+  except:
+    return ""
 
 client_interface_name = 'apcli0'
 
