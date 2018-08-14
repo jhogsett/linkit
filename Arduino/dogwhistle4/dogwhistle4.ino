75e74551d225eaaa7e09862735eded2d7878d4ab
Jerry Hogsett <jerry@usertesting.com>
2018-08-14 09:35:04 -0700

    effective dog bark stoppng

---
+++ Arduino/dogwhistle4/dogwhistle4.ino
@@ -0,0 +1,24 @@
+
+#define RANDOM_SEED_SAMPLES 16
+#define RANDOM_SEED_PIN 0
+
+void setup() 
+{
+  int seed = analogRead(RANDOM_SEED_PIN);
+  for(byte i = 0; i < RANDOM_SEED_SAMPLES; i++)
+    seed = (seed << 1) ^ analogRead(0);
+  randomSeed(seed);
+}
+
+
+void loop() 
+{
+
+  int t = random(3000);
+  tone(8, t + 13000);
+  int d = random(1000);
+  delay(d + 1000);
+
+}
+
+
