a3083e1138c664baa095c2b37a70972af662da2b
jerry hogsett <jhogsett@gmail.com>
2018-12-20 17:07:09 +0000

    add final command arg"

--- python/awakening.py
+++ python/awakening.py
@@ -9,7 +9,7 @@ import led_command as lc
 from kelvin_to_rgb import convert_K_to_RGB
 from subprocess import call
 
-global app_description, verbose_mode, kelvin_start, kelvin_end, num_steps, brightness_start, brightness_end, step_time, kelvin_step, brightness_step, kelvin_current, brightness_current, dry_run, factor, send_to_led, send_to_udp, step_number
+global app_description, verbose_mode, kelvin_start, kelvin_end, num_steps, brightness_start, brightness_end, step_time, kelvin_step, brightness_step, kelvin_current, brightness_current, dry_run, factor, send_to_led, send_to_udp, step_number, final_command
 app_description = None
 verbose_mode = None
 kelvin_start = None
@@ -23,6 +23,7 @@ dry_run = None
 send_to_led = None
 send_to_udp = None
 step_number = None
+final_command = None
 
 kelvin_step = None
 brightness_step = None
@@ -32,21 +33,22 @@ brightness_current = None
 # add factor, dry run
 
 def get_options():
-    global app_description, verbose_mode, kelvin_start, kelvin_end, num_steps, brightness_start, brightness_end, step_time, dry_run, factor, send_to_led, send_to_udp
+    global app_description, verbose_mode, kelvin_start, kelvin_end, num_steps, brightness_start, brightness_end, step_time, dry_run, factor, send_to_led, send_to_udp, final_command
     app_description = "Awakening Sunrise - Apollo Lighting System v.0.0 12-0-18"
 
     parser = argparse.ArgumentParser(description=app_description)
     parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="display verbose info (False)")
     parser.add_argument("-n", "--num-steps", type=int, dest="num_steps", default=30, help="number of steps (30)")
     parser.add_argument("-t", "--step_time", type=int, dest="step_time", default=60, help="seconds for each step (60)")
-    parser.add_argument("-ks", "--kelvin-start", type=int, dest="kelvin_start", default=1000, help="starting color temperature K (1000)")
-    parser.add_argument("-ke", "--kelvin-end", type=int, dest="kelvin_end", default=5600, help="ending color temperature K (5600)")
+    parser.add_argument("-ks", "--kelvin-start", type=int, dest="kelvin_start", default=1000, help="starting color temperature K (500)")
+    parser.add_argument("-ke", "--kelvin-end", type=int, dest="kelvin_end", default=5600, help="ending color temperature K (5000)")
     parser.add_argument("-bs", "--brightness-start", type=int, dest="brightness_start", default=1, help="starting brightness %% (1)")
     parser.add_argument("-be", "--brightness-end", type=int, dest="brightness_end", default=30, help="ending brightness %% (30)")
     parser.add_argument("-r", "--dry-run", dest="dry_run", action="store_true", help="process the awakening values but don't send the results (False)")
     parser.add_argument("-f", "--rgb-factor", type=int, dest="factor", default=10, help="rgb value divisor (10)")
     parser.add_argument("-l", "--send-to-led", dest="send_to_led", action="store_true", help="send the awakening values to the LED device (False)")
     parser.add_argument("-b", "--broadcast", dest="send_to_udp", action="store_true", help="send the awakening values to the network via UDP (False)")
+    parser.add_argument("-c", "--final-command", dest="final_command", default="app:10:run", help="LED command string to send when done (app:10:run)")
 
     args = parser.parse_args()
     verbose_mode = args.verbose
@@ -60,6 +62,7 @@ def get_options():
     factor = args.factor
     send_to_led = args.send_to_led
     send_to_udp = args.send_to_udp
+    final_command = args.final_command
 
 def validate_options():
     pass
@@ -86,7 +89,7 @@ def initialize():
     validate_options()
     ui.begin(verbose_mode)
     introduction()
-    lc.begin(False) #verbose_mode)
+    lc.begin(verbose_mode)
 
     kelvin_step = (kelvin_end-kelvin_start) / (num_steps * 1.0)
     brightness_step = (brightness_end-brightness_start) / (num_steps * 1.0)
@@ -98,6 +101,29 @@ def initialize():
     brightness_current = brightness_start
     step_number = 0
 
+    on_start()
+
+def on_start():
+    send_to_led_device("app")
+    broadcast("app")
+
+def on_complete():
+    send_to_led_device(final_command)
+    broadcast(final_command)
+
+def send_to_led_device(command_str):
+    if send_to_led:
+        ui.report_verbose("sending to LED device: " + command_str)
+        if not dry_run:
+            lc.command(command_str)
+
+def broadcast(command_str):
+    if send_to_udp:
+        command_line = "/root/dev/linkit/python/udp_send.py " + command_str + " &"
+        ui.report_verbose("broadcasting: " + command_line)
+        if not dry_run:
+            call(command_line, shell=True)
+
 ############################################################################
 
 def loop():
@@ -108,13 +134,11 @@ def loop():
 
 def do_broadcast():
     r, g, b = convert_K_to_RGB(kelvin_current)
-
     r = r / factor
     g = g / factor
     b = b / factor
 
     command_str = str(int(brightness_current)) + ":lev:" + str(int(r)) + "," + str(int(g)) + "," + str(int(b)) + ":rgb:flo:flu"
-    command_line = "/root/dev/linkit/python/udp_send.py " + command_str + " &"
 
     if step_number == 0:
         ui.report_info("Initial Step")
@@ -123,15 +147,11 @@ def do_broadcast():
 
     ui.report_info("Current color temperature: " + str(int(kelvin_current)) + "K")
     ui.report_info("Current brightness: " + str(int(brightness_current)) + "%%")
-    print
-    ui.report_verbose(command_line)
-    ui.report_verbose()
 
-    if not dry_run:
-        if send_to_led:
-            lc.command(command_str)
-        if send_to_udp:
-            call(command_line, shell=True)
+    send_to_led_device(command_str)
+    broadcast(command_str)
+
+    print
 
 def next_step():
     global kelvin_current, brightness_current, step_number
@@ -142,6 +162,7 @@ def next_step():
 def exit_if_done():
     if kelvin_current > kelvin_end:
         ui.report_info("Awakening complete!")
+        on_complete()
         sys.exit()
 
 def wait_step():
