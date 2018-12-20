1594c2a677780563a308b2c06dd496c4dcc12f37
jerry hogsett <jhogsett@gmail.com>
2018-12-20 03:30:37 +0000

    fix support for sunsets, add time offset

--- python/sunrise.py
+++ python/sunrise.py
@@ -6,8 +6,10 @@ import time
 from subprocess import call
 import argparse
 from requests.exceptions import ConnectionError
+import terminal_colors as tc
+import app_ui as ui
 
-global app_description, verbose_mode, api_key, zipcode, update_freq, timezone_offset, sleep_time, last_command_time, dryrun_mode, command_line, dryrun_time, command_sent, target_time, dryrun_time
+global app_description, verbose_mode, api_key, zipcode, update_freq, timezone_offset, sleep_time, last_command_time, dryrun_mode, command_line, dryrun_time, command_sent, target_time, dryrun_time, target_times
 app_description = None
 verbose_mode = None
 api_key = None
@@ -21,10 +23,11 @@ dryrun_time = None
 command_sent = True
 target_time = None
 dryrun_time = None
+target_times = []
 
 def get_options():
-    global app_description, verbose_mode, api_key, zipcode, update_freq, timezone_offset, dryrun_mode, command_line
-    app_description = "Sunrise Commander - Apollo Lighting System v.0.0 9-0-18"
+    global app_description, verbose_mode, api_key, zipcode, update_freq, timezone_offset, dryrun_mode, command_line, event_type, trigger_offset
+    app_description = "Sunrise/Sunset Commander - Apollo Lighting System v.1.0 12-19-18"
 
     parser = argparse.ArgumentParser(description=app_description)
     parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="display verbose info (False)")
@@ -33,6 +36,8 @@ def get_options():
     parser.add_argument("-f", "--update_frequency", type=int, dest="update_freq", default=15, help="update freqency (seconds) (15)")
     parser.add_argument("-t", "--timezone_offset", type=int, dest="timezone_offset", default=8, help="timezone offset from UTC (hours) (8)")
     parser.add_argument("-r", "--dryrun", dest="dryrun", action="store_true", help="use a fake event time (False)")
+    parser.add_argument("-e", "--event_type", dest="event_type", default="sunrise", help="triggering event 'sunset' or 'sunrise' (sunrise)")
+    parser.add_argument("-o", "--trigger_offset", type=int, dest="trigger_offset", default=0, help="+/- minutes offset for triggering (0)")
     parser.add_argument("command_line", nargs=argparse.REMAINDER, help="command line to run at sunrise (None)")
     args = parser.parse_args()
     verbose_mode = args.verbose
@@ -41,29 +46,44 @@ def get_options():
     update_freq = args.update_freq
     timezone_offset = args.timezone_offset
     dryrun_mode = args.dryrun
+    event_type = args.event_type
+    trigger_offset = args.trigger_offset
     command_line = args.command_line
 
 def validate_options():
     global command_line
     if api_key == None:
-        print "api_key must be specified"
-        sys.exit()
+        sys.exit("\napi_key must be specified\n")
     if zipcode == None:
-        print "zipcode must be specified"
-        sys.exit()
+        sys.exit("\nzipcode must be specified\n")
     if len(command_line) == 0:
-	print "command_line must be specified"
-	sys.exit()
+	sys.exit("\ncommand_line must be specified\n")
+    if not event_type in ["sunrise", "sunset"]:
+        sys.exit("\nevent type must be one of: sunrise, sunset\n")
     command_line = " ".join(command_line)
 
-def setup():
+def introduction():
+    ui.app_description(app_description)
+
+    ui.report_verbose("verbose mode")
+    ui.report_verbose("zip code: " + zipcode)
+    ui.report_verbose("update frequency: " + str(update_freq) + " seconds")
+    ui.report_verbose("timezone offset: " + str(timezone_offset) + " hours")
+    ui.report_verbose("dry run: " + str(dryrun_mode))
+    ui.report_verbose("event type: " + event_type)
+    ui.report_verbose("trigger offset: " + str(trigger_offset) + " minutes")
+    ui.report_verbose("command line: " + command_line)
+    ui.report_verbose()
+
+def initialize():
     get_options()
     validate_options()
     wx.begin(api_key, zipcode, timezone_offset)
+    ui.begin(verbose_mode)
+    introduction()
 
 def do_command():
-    if verbose_mode:
-        print command_line
+    ui.report_verbose(command_line)
     call(command_line, shell=True)
 
 
@@ -82,53 +102,77 @@ def set_dryrun_time():
     data = wx.get_daily_data()
     current_time = int(wx.current_time())
     dryrun_time = current_time + (3 * update_freq)
-    print "dry run: setting sunset time to " + wx.format_unix_timestamp(dryrun_time)
+    ui.report_warn("dry run: setting sunset time to " + wx.format_unix_timestamp(dryrun_time))
 
 def loop():
-    global last_command_time, target_time, dryrun_time, command_sent
+    global last_command_time, target_time, dryrun_time, command_sent, target_times
     try:
         data = wx.get_daily_data()
         current_time = int(wx.current_time())
-        event_time = int(wx.daily_sunrise(data))
 
-	if dryrun_mode:
-	    if dryrun_time == None:
+        if event_type == "sunrise":
+            event_time = int(wx.daily_sunrise(data))
+        else:
+            event_time = int(wx.daily_sunset(data))
+        event_time += trigger_offset * 60
+
+        if dryrun_mode:
+            if dryrun_time == None:
                 set_dryrun_time()
             event_time = dryrun_time
 
-        print "Sunrise: " + wx.format_unix_timestamp(event_time)
-        print "Current time: " + wx.format_unix_timestamp(current_time)
-
-        if target_time != None and current_time > target_time:
-            print "target time is in the past"
-	    if not command_sent:
-                print "sending command"
-                do_command()
-                command_sent = True
-		if dryrun_mode:
-                     set_dryrun_time()
-	if target_time == None or target_time < event_time:
-	    print "target time not set or in the past"
-            if event_time <= current_time:
-                print "sunrise time still in the past"
+        if event_time not in target_times:
             if event_time > current_time:
-	        target_time = event_time
-                print "new target time: " + wx.format_unix_timestamp(target_time)
-                command_sent = False
+                target_times.append(event_time)
+                ui.report_info_alt("New target time added to list: " + wx.format_unix_timestamp(event_time))
+            else:
+                ui.report_error("New target time is in the past: " + wx.format_unix_timestamp(event_time))
+
+        ui.report_info("Current time: " + wx.format_unix_timestamp(current_time))
+
+        for target_time in target_times:
+            ui.report_info(event_type.capitalize() + " time: " + wx.format_unix_timestamp(target_time))
+
+        new_target_times = []
+        command_sent = False
+        send_trigger_time = 0
+        margin_time = 60
+        for target_time in target_times:
+            if current_time >= target_time:
+                # target time has been reached
+
+                # see if this event is close to a triggering event this round
+                if send_trigger_time != 0:
+
+                    # this target time must exceed the triggering time 
+                    # by margin seconds to not be skipped
+                    if target_time < send_trigger_time + margin_time:
+                        # this target time is within the margin seconds 
+                        # of the triggering event so skip it
+                        ui.report_info("skipping near duplicate event")
+                        continue
+                if command_sent == False:
+                    ui.report_info("sending command")
+                    do_command()
+                    command_sent = True
+                    send_trigger_time = target_time
+                    if dryrun_mode:
+                        set_dryrun_time()
+            else:
+                new_target_times.append(target_time)
+        target_times = new_target_times
         print
 
     except ConnectionError as e:
-        if verbose_mode:
-            print("ignoring error: ", str(e))
+        ui.report_verbose("ignoring error: " + str(e))
 
     except KeyError as e:
-        if verbose_mode:
-            print("ignoring error: ", str(e))
+        ui.report_verbose("ignoring error: " + str(e))
     finally:
         time.sleep(update_freq)
 
 if __name__ == '__main__':
-    setup()
+    initialize()
     while True:
         try:
             loop()
