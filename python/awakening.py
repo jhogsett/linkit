c5e1d4e4b2c58053f1ba28a7441fed4b1a9e098f
jerry hogsett <jhogsett@gmail.com>
2018-12-20 05:01:19 +0000

    awakening light program

---
+++ python/awakening.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python
+
+import sys
+import time
+import argparse
+import terminal_colors as tc
+import app_ui as ui
+import led_command as lc
+from kelvin_to_rgb import convert_K_to_RGB
+from subprocess import call
+
+global app_description, verbose_mode, kelvin_start, kelvin_end, num_steps, brightness_start, brightness_end, step_time, kelvin_step, brightness_step, kelvin_current, brightness_current, dry_run, factor
+app_description = None
+verbose_mode = None
+kelvin_start = None
+kelvin_end = None
+num_steps = None
+brightness_start = None
+brightness_end = None
+step_time = None
+factor = None
+dry_run = None
+
+kelvin_step = None
+brightness_step = None
+kelvin_current = None
+brightness_current = None
+
+# add factor, dry run
+
+def get_options():
+    global app_description, verbose_mode, kelvin_start, kelvin_end, num_steps, brightness_start, brightness_end, step_time, dry_run, factor
+    app_description = "Awakening Sunrise - Apollo Lighting System v.0.0 12-0-18"
+
+    parser = argparse.ArgumentParser(description=app_description)
+    parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="display verbose info (False)")
+    parser.add_argument("-n", "--num-steps", type=int, dest="num_steps", default=30, help="number of steps (30)")
+    parser.add_argument("-t", "--step_time", type=int, dest="step_time", default=60, help="seconds for each step (60)")
+    parser.add_argument("-ks", "--kelvin-start", type=int, dest="kelvin_start", default=1000, help="starting color temperature K (1000)")
+    parser.add_argument("-ke", "--kelvin-end", type=int, dest="kelvin_end", default=5600, help="ending color temperature K (5600)")
+    parser.add_argument("-bs", "--brightness-start", type=int, dest="brightness_start", default=1, help="starting brightness %% (1)")
+    parser.add_argument("-be", "--brightness-end", type=int, dest="brightness_end", default=30, help="ending brightness %% (30)")
+    parser.add_argument("-r", "--dry_run", dest="dry_run", action="store_true", help="process the script but don't actually program the device (False)")
+    parser.add_argument("-f", "--rgb-factor", type=int, dest="factor", default=10, help="rgb value divisor (10)")
+
+    args = parser.parse_args()
+    verbose_mode = args.verbose
+    num_steps = args.num_steps
+    step_time = args.step_time
+    kelvin_start = args.kelvin_start
+    kelvin_end = args.kelvin_end
+    brightness_start = args.brightness_start
+    brightness_end = args.brightness_end
+    dry_run = args.dry_run
+    factor = args.factor
+
+def validate_options():
+    pass
+
+def introduction():
+    ui.app_description(app_description)
+
+    ui.report_verbose("verbose mode")
+    ui.report_verbose("number of steps: " + str(num_steps))
+    ui.report_verbose("step time: " + str(step_time) + " seconds")
+    ui.report_verbose("starting color temperature: " + str(kelvin_start) + "K")
+    ui.report_verbose("ending color temperature: " + str(kelvin_end) + "K")
+    ui.report_verbose("starting brightness: " + str(brightness_start) + "%")
+    ui.report_verbose("ending brightness: " + str(brightness_end) + "%")
+    ui.report_verbose("rgb factor: " + str(factor))
+    ui.report_verbose("dry run: " + str(dry_run))
+    ui.report_verbose()
+
+def initialize():
+    global kelvin_step, brightness_step, kelvin_current, brightness_current
+    get_options()
+    validate_options()
+    ui.begin(verbose_mode)
+    introduction()
+    lc.begin(False) #verbose_mode)
+
+    kelvin_step = (kelvin_end-kelvin_start) / (num_steps * 1.0)
+    brightness_step = (brightness_end-brightness_start) / (num_steps * 1.0)
+
+    ui.report_verbose("color temperature step: " + str(kelvin_step))
+    ui.report_verbose("brightness step: " + str(brightness_step))
+
+    kelvin_current = kelvin_start
+    brightness_current = brightness_start
+
+############################################################################
+
+def loop():
+    do_broadcast()
+    next_step()
+    exit_if_done()
+    wait_step()
+
+def do_broadcast():
+    r, g, b = convert_K_to_RGB(kelvin_current)
+
+    r = r / factor
+    g = g / factor
+    b = b / factor
+
+    command_str = str(int(brightness_current)) + ":lev:" + str(int(r)) + "," + str(int(g)) + "," + str(int(b)) + ":rgb:flo:flu"
+    command_line = "/root/dev/linkit/python/udp_send.py " + command_str + " &"
+    ui.report_verbose(command_line)
+
+    if not dry_run:
+        lc.command(command_str)
+        call(command_line, shell=True)
+
+def next_step():
+    global kelvin_current, brightness_current
+    kelvin_current += kelvin_step
+    brightness_current += brightness_step
+
+def exit_if_done():
+    if kelvin_current >= kelvin_end:
+        ui.report_info("Awakening complete!")
+        sys.exit()
+
+def wait_step():
+    time.sleep(step_time)
+
+############################################################################
+############################################################################
+
+if __name__ == '__main__':
+    initialize()
+    while True:
+        try:
+            loop()
+        except KeyboardInterrupt:
+            sys.exit("\nExiting...\n")
+        except Exception:
+            raise
+
