f876c1d8ad00c090fb2cc5bf80be997f8c8440d9
Jerry Hogsett <jhogsett@gmail.com>
2019-02-15 04:45:28 +0000

    add a couple functions;

--- python/utils.py
+++ python/utils.py
@@ -2,6 +2,7 @@
 
 
 import os
+from subprocess import call
 
 def load_file(filename, default_ext=".???"):
     file_lines = []
@@ -19,4 +20,26 @@ def load_file(filename, default_ext=".???"):
 
     return file_lines
 
+def get_pair(line, joiner="-"):
+  line = line.strip()
+  args = line.split(joiner)
+  if len(args) == 0:
+    return [None, None]
+  elif len(args) == 1:
+    return [args[0].strip(), None]
+  elif len(args) == 2:
+    return [args[0].strip(), args[1].strip()]
+
+def strip_comments(lines, comment_marker="#"):
+  new_lines = []
+  for line in lines:
+    if comment_marker in line:
+      parts = line.split(comment_marker)
+      new_lines.append(parts[0].strip())
+    else:
+      new_lines.append(line)
+  return new_lines
+
+def run_command(command_line):
+      call(command_line, shell=True)
 
