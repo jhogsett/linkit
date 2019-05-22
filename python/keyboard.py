60b1f7ff79c5e37cf7cf0d38df17e162e2ff557a
Jerry Hogsett <jhogsett@gmail.com>
2019-05-22 03:44:27 +0000

    move some things to modules

---
+++ python/keyboard.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+import time
+
+import app_ui as ui
+import utils 
+import led_command as lc
+
+global poll_frequency, long_press, too_short_press, short_press_tone, short_press_duration, alt_press_tone, alt_press_duration, long_press_tone, long_press_duration
+global sleep_time, key_callback, verbose_mode
+
+verbose_mode = False
+poll_frequency = 20
+long_press = 50
+too_short_press = 3
+short_press_tone = 2800
+short_press_duration = 50
+alt_press_tone = 2200
+alt_press_duration = 50
+long_press_tone = 3100
+long_press_duration = 200
+sleep_time = 1.0 / poll_frequency
+key_callback = None
+
+def begin(callback=None, verbose_mode_=False):
+    global key_callback, verbose_mode
+
+    if callback == None:
+        callback = default_callback
+    key_callback = callback
+
+    verbose_mode = verbose_mode_   
+
+    lc.begin(verbose_mode)
+    lc.stop_all()
+
+    enable_keyboard()
+    store_long_beep_on_device()
+
+def default_callback(key, long_press):
+    print "key: {} long press: {}".format(key, long_press)
+    long_beep() if long_press else short_beep()
+
+def poll_forever():
+    while(True):
+        poll()
+        poll_wait()
+
+def poll_wait():
+    time.sleep(sleep_time)
+
+def poll():
+    result = lc.command_str("4:key")
+    args = result.split(",")
+    if len(args) < 2:
+        return
+    key = int(args[0])
+    if key:
+       count = int(args[1])
+       if count <= too_short_press:
+           return
+       key_callback(key, True if count >= long_press else False)
+
+# ========================================
+
+def enable_keyboard():
+    lc.command("4,0:cfg:1:cnt:3:cnt")
+
+def store_long_beep_on_device():
+    lc.command_str("3,-1,0:key:0:set:" + str(long_press_tone) + "," + str(long_press_duration) + ":ton")
+
+def short_beep():
+    lc.command(str(short_press_tone) + "," + str(short_press_duration) + ":ton")
+
+def alt_beep():
+    lc.command(str(alt_press_tone) + "," + str(alt_press_duration) + ":ton")
+
+def long_beep():
+    lc.command(str(long_press_tone) + "," + str(long_press_duration) + ":ton")
+
---
+++ python/multicast.py
@@ -0,0 +1,125 @@
+import socket
+from socket import error as socket_error
+import struct
+import threading
+import time
+
+import app_ui as ui
+
+global host_name, verbose_mode, multicast_group_ip, multicast_port, timeout_in_seconds, multicast_group, num_times, no_keys, msg_delay
+global responses, background_threads
+responses = {}
+background_threads = []
+
+def begin(host_name_, verbose_mode_=False, multicast_group_ip_=None, multicast_port_=None, timeout_in_seconds_=None, multicast_group_=None, num_times_=None, msg_delay_=None):
+    global host_name, verbose_mode, multicast_group_ip, multicast_port, timeout_in_seconds, multicast_group, num_times, no_keys, msg_delay
+
+    host_name = host_name_
+    verbose_mode = verbose_mode_
+    multicast_group_ip = multicast_group_ip_ or '224.3.29.71'
+    multicast_port = multicast_port_         or 10000
+    timeout_in_seconds = timeout_in_seconds_ or 0.1
+    num_times = num_times_                   or 15
+    no_keys = False
+    msg_delay = msg_delay_                   or 0.001
+
+    multicast_group = (multicast_group_ip, multicast_port)
+
+    ui.begin(verbose_mode)
+    ui.report_verbose("verbose mode enabled")
+
+def conclude():
+    wait_for_active_threads()
+
+def broadcast(message, targets=None):
+    message = create_key(message, targets)
+    send_background_message(message)
+
+def received():
+    return responses
+
+# ========================================
+
+def create_key(message, targets):
+    key = host_name + "/" + str(time.time())
+    if targets:
+        key = key + "/" + targets
+    key = key + ";" + message
+    return key
+
+# ========================================
+
+def cast_socket():
+    # Create the datagram socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Set a timeout so the socket does not block indefinitely when trying
+    # to receive data.
+    sock.settimeout(timeout_in_seconds)
+
+    # Set the time-to-live for messages to 1 so they do not go past the
+    # local network segment.
+    ttl = struct.pack('b', 1)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+    return sock
+
+def send_socket_message(sock, message, times):
+    global responses
+    responses = {}
+
+    for n in range(0, times):
+        ui.report_verbose('sending "%s"' % message)
+        sent = sock.sendto(message, multicast_group)
+
+        while True:
+            try:
+                data, server = sock.recvfrom(256)
+            except socket.timeout:
+                break
+            ip = server[0].strip("'")
+            responses[ip] = data
+            ui.report_verbose('received "%s" from %s' % (data, ip))
+
+        if n < (times - 1):
+            time.sleep(msg_delay * (2 ** n))
+
+def send_message(message):
+    sock = cast_socket()
+    send_socket_message(sock, message, num_times)
+    sock.close()
+
+def handle_background_message(message):
+    send_message(message)
+    thread = threading.current_thread()
+    background_threads.remove(thread)
+    ui.report_verbose("terminating thread: " + str(thread))
+
+def send_background_message(message):
+    thread = threading.Thread(target=handle_background_message, args=(message,))
+    ui.report_verbose("new thread: " + str(thread))
+    background_threads.append(thread)
+    thread.start()
+
+def wait_for_active_threads():
+    if(len(background_threads) > 0):
+        ui.report_warn("waiting for active threads to terminate...")
+        for t in background_threads:
+            t.join()
+
+############################################################################
+############################################################################
+
+#if __name__ == '__main__':
+#  setup()
+#  try:
+#    run()
+#  except KeyboardInterrupt:
+#    pass
+#    sys.exit("\nExiting...\n")
+#
+#  finally:
+#    wait_for_active_threads()
+#    conclude()
+#    sys.exit("\nExiting...\n")
+
