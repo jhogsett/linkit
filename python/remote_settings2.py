4da4ae977a7286610c50a3bb09bb625b0fc79fb7
Jerry Hogsett <jhogsett@gmail.com>
2019-05-29 07:34:23 +0000

    for mini remote

---
+++ python/remote_settings2.py
@@ -0,0 +1,17 @@
+short_press = [
+    { "macro": 10,            "effect": "red:sfd:1:cpy:10:run" },
+    { "macro": 11,            "effect": "grn:sfd:1:cpy:11:run" },
+    { "color": "wht",         "effect": "wht:ffd:flo" },
+    { "cmd": "0:lev",         "effect": "0:lev" },
+    { "cmd": "-3:lev",        "effect": "-3:lev" }
+]
+
+long_press = [
+    { "macro": 12,            "effect": "blu:sfd:1:cpy:12:run" },
+    { "macro": 13,            "effect": "pur:sfd:1:cpy:13:run" },
+    { "color": "tun",         "effect": "wht:ffd:flo" },
+    { "random": "0,-1:rnd", "effect": "0,-1:rnd:flu" },
+    { "raw": "stp",           "effect": "era:flu" }
+]
+
+
