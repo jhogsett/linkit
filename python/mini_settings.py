457b438c81051cf1a135fa1c31a15c087d1f9df5
Jerry Hogsett <jhogsett@gmail.com>
2019-05-30 08:01:10 +0000

    tweak commands

--- python/mini_settings.py
+++ python/mini_settings.py
@@ -1,16 +1,16 @@
 short_press = [
-    { "macro": 10,            "effect": "red:ffd:1:cpy" },
-    { "macro": 11,            "effect": "grn:ffd:1:cpy" },
-    { "cmd": "0:lev",         "effect": "0:lev" },
-    { "color": "wht",         "effect": "wht:ffd:flo" },
-    { "raw": "stp",           "effect": "era:flu" }
+    { "macro": 10,            "effect": "red:ffd:flo:flu" },
+    { "macro": 11,            "effect": "grn:ffd:flo:flu" },
+    { "cmd": "-3:lev",        "effect": "-3:lev" },
+    { "color": "tun",         "effect": "tun:flo:flu" },
+    { "raw": "stp:0:lev",     "effect": "0:lev:-1:sch:era:flu" }
 ]
 
 long_press = [
-    { "macro": 12,            "effect": "blu:ffd:1:cpy" },
-    { "macro": 13,            "effect": "pur:ffd:1:cpy:13:run" },
-    { "cmd": "-3:lev",        "effect": "-3:lev" },
-    { "color": "tun",         "effect": "wht:ffd:flo" },
-    { "random": "0,-1:rnd", "effect": "0,-1:rnd:flu" }
+    { "macro": 12,            "effect": "blu:ffd:flo:flu" },
+    { "macro": 13,            "effect": "pur:ffd:flo:13:run" },
+    { "cmd": "-4:lev",        "effect": "-4:lev" },
+    { "color": "neo",         "effect": "neo:flo:flu" },
+    { "random": "0,-1:rnd",   "effect": "0,-1:rnd" }
 ]
 
